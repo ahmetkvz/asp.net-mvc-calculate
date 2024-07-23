68a4d7398d2854705e24b1d99c137e394822d1c2
ahmetkvz <ahmet2525kavaz@gmail.com>
2024-07-23 13:21:33 +0300

    asp.net mvc calculate

---
+++ eskiyeniDeneme/Migrations/202407191249202_UpdateCalculationModel.cs
@@ -0,0 +1,16 @@
+﻿namespace eskiyeniDeneme.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateCalculationModel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ eskiyeniDeneme/Models/calc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace eskiyeniDeneme.Models
+{
+    public class calc
+    {
+        [Key]
+        public int Id { get; set; }
+
+       
+        public double Sayi1 { get; set; }
+
+      
+        public double Sayi2 { get; set; }
+
+        public string Islem { get; set; }
+
+       
+        public double Sonuc { get; set; }
+
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+    }
+}
